15827e99c978ef46df3c717168e5fa030fee9818
Nick Ezop <nick.ezop@gmail.com>
2022-10-03 14:16:38 -0400

    Saving work

---
+++ PyBank/main.py
@@ -0,0 +1,44 @@
+#Import os
+import os
+
+#import csv reader
+import csv
+from statistics import mean
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+financial_analysis = open(csvpath)
+
+total = 0
+month = 0
+net_total = 0
+pre_prof = 0
+changes = 0
+average_changes = 0
+greatest_profit = 0
+lowest_profit = 0
+
+for row in financial_analysis:
+    month += 1
+    total = int(row['Profit/Losses'])
+    net_total += total
+    changes = net_total - pre_prof
+    average_changes = mean(changes)
+
+output = f'''
+Financial Analysis
+----------------------------
+Total months: {month}
+Total: {net_total:,.2f}
+Average Change: {average_changes}
+
+'''
+
+print(output)
+
+
+
+
+
+
+
+
---
+++ PyPoll/main.py
@@ -0,0 +1,8 @@
+import os
+
+import csv
+csvpath = os.path.join('Resources', 'election_data.csv')
+election_results = open(csvpath)
+
+
+
